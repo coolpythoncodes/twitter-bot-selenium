07c9cfedf7c3f97ffe2149ec2c3d2a3722f5bf42
coolpythoncodes <chijiokeibe15@gmail.com>
2020-04-11 02:12:24 +0000

    first commit

---
+++ twitter_bot_selenium.py
@@ -0,0 +1,11 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time 
+import io
+import csv
+import os
+import re
+import config # config is a python file that has your login details to twitter
+
